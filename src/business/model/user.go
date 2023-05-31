051847e6b891aff037fe790098c285e622ad31b1
alitdarmaputra <alitdarmaputra@gmail.com>
2023-06-01 00:07:25 +0700

    add verified at column

--- src/business/model/user.go
+++ src/business/model/user.go
@@ -1,13 +1,16 @@
 package entity
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	Email      string `gorm:"column:email"`
-	Password   string `gorm:"column:password"`
-	Name       string `gorm:"column:name"`
-	ProfileImg string `gorm:"column:profile_img"`
+	Email      string    `gorm:"column:email"`
+	Password   string    `gorm:"column:password"`
+	Name       string    `gorm:"column:name"`
+	ProfileImg string    `gorm:"column:profile_img"`
+	VerifiedAt time.Time `gorm:"column:verified_at"`
 }
