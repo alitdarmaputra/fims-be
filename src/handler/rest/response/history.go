e01678ab2b2d239ba556d7f9f00c3abd45f405f6
alitdarmaputra <alitdarmaputra@gmail.com>
2023-06-08 22:57:40 +0700

    update history response

--- src/handler/rest/response/history.go
+++ src/handler/rest/response/history.go
@@ -7,16 +7,17 @@ import (
 )
 
 type HTTPHistoryResponse struct {
-	NodeId       uint      `json:"id"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedBy    string    `json:"updated_by"`
-	Description  string    `json:"description"`
-	HistoryType  string    `json:"history_type"`
-	FigmaUrl     string    `json:"figma_url"`
-	SnapshotPath string    `json:"snapshot_path"`
-	StatusFrom   string    `json:"status_from"`
-	StatusTo     string    `json:"status_to"`
-	NodeTitle    string    `json:"node_title"`
+	NodeId           uint      `json:"node_id"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedBy        string    `json:"updated_by"`
+	UpdatedByProfile string    `json:"updated_by_profile"`
+	Description      string    `json:"description"`
+	HistoryType      string    `json:"history_type"`
+	FigmaUrl         string    `json:"figma_url"`
+	SnapshotPath     string    `json:"snapshot_path"`
+	StatusFrom       string    `json:"status_from"`
+	StatusTo         string    `json:"status_to"`
+	NodeTitle        string    `json:"node_title"`
 }
 
 func ToHistoryResponse(history model.History) HTTPHistoryResponse {
@@ -24,6 +25,7 @@ func ToHistoryResponse(history model.History) HTTPHistoryResponse {
 
 	response.CreatedAt = history.CreatedAt
 	response.UpdatedBy = history.User.Name
+	response.UpdatedByProfile = history.User.ProfileImg
 	response.Description = history.Description
 	response.HistoryType = history.HistoryType
 	response.NodeTitle = history.Node.Title
